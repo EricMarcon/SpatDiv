da3b9d367a93a9c99039a73ebb55f41acf054e3f
Eric Marcon <eric.marcon@agroparistech.fr>
2022-01-28 17:06:13 +0100

    Simulated sizes

--- R/SpCommunity.R
+++ R/SpCommunity.R
@@ -5,17 +5,30 @@
 #'
 #' @param n The number of communities to draw.
 #' @param size The number of individuals to draw in each community.
-#' @param NorP A numeric vector or a two-column matrix. Contains either abundances or probabilities. Two-column matrices should contain the observed abundances (or probabilities) in the first column and the expected ones in the second column, to allow using beta diversity functions.
-#' @param BootstrapMethod  The method used to obtain the probabilities to generate bootstrapped communities from observed abundances. If `"Marcon"`, the probabilities are simply the abundances divided by the total number of individuals \insertCite{Marcon2012a}{SpatDiv}. If `"Chao2013"` or `"Chao2015"` (by default), a more sophisticated approach is used (see [as.ProbaVector]) following \insertCite{Chao2013;textual}{SpatDiv} or \insertCite{Chao2015;textual}{SpatDiv}.
+#' @param NorP A numeric vector or a two-column matrix. 
+#' Contains either abundances or probabilities. 
+#' Two-column matrices should contain the observed abundances (or probabilities) in the first column 
+#' and the expected ones in the second column, to allow using beta diversity functions.
+#' @param BootstrapMethod  The method used to obtain the probabilities to generate bootstrapped communities 
+#' from observed abundances. 
+#' If `"Marcon"`, the probabilities are simply the abundances divided by the total number of individuals \insertCite{Marcon2012a}{SpatDiv}. 
+#' If `"Chao2013"` or `"Chao2015"` (by default), a more sophisticated approach is used (see [as.ProbaVector]) 
+#' following \insertCite{Chao2013;textual}{SpatDiv} or \insertCite{Chao2015;textual}{SpatDiv}.
 #' @param S The number of species.
-#' @param Distribution The distribution of species frequencies. May be `"lnorm"` (log-normal), `"lseries"` (log-series), `"geom"` (geometric) or `"bstick"` (broken stick).
+#' @param Distribution The distribution of species frequencies. 
+#' May be `"lnorm"` (log-normal), `"lseries"` (log-series), `"geom"` (geometric) or `"bstick"` (broken stick).
 #' @param sd The simulated distribution standard deviation. For the log-normal distribution, this is the standard deviation on the log scale.
 #' @param prob The probability of success in each trial.
 #' @param alpha Fisher's alpha.
-#' @param Spatial TODO
+#' @param Spatial The spatial distribution of points. 
+#' May be "Binomial" (a completely random point pattern except for its fixed number of points).
 #' @param win The window containing the point pattern. It is an [owin] object.
 #' @param Species A vector of characters or of factors containing the possible species.
-#' @param Sizes TODO
+#' @param Sizes The distribution of point sizes.
+#' May be "Uniform" for a uniform distribution between `MinSize` and `MaxSize`.
+#' By default, all sizes are 1.
+#' @param MinSize The minimum size in a uniform distribution.
+#' @param MaxSize The maximum size in a uniform distribution.
 #' @inheritParams EntAccum
 #'
 #' @return A [wmppp.object], with `PointType` values as species names if `n`=1.
@@ -33,7 +46,7 @@ function(n, size = sum(NorP), NorP = 1, BootstrapMethod = "Chao2015",
          S = 300, Distribution = "lnorm", sd = 1, prob = 0.1, alpha=40,
          Spatial = "Binomial", win=spatstat.geom::owin(),
          Species = NULL,
-         Sizes = "Uniform",
+         Sizes = "Uniform", MinSize = 1, MaxSize = 1,
          CheckArguments = TRUE)
 {
   if (CheckArguments)
@@ -47,7 +60,7 @@ function(n, size = sum(NorP), NorP = 1, BootstrapMethod = "Chao2015",
 
   # Sizes
   if (Sizes == "Uniform") {
-    Sizes <- rep(1, size)
+    Sizes <- runif(size, min=MinSize, max=MaxSize)
   }
 
   # Spatial distribution
